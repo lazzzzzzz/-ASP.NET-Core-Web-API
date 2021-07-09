1217a44ab38e0fbaff278a5aae0288e8779d5dc0
lazzzzzzz <zeygman@gmail,com>
2021-07-09 18:46:19 +0500

    added lesson4

--- MetricsManager/MetricsAgent/DAL/RamMetricsRepository.cs
+++ MetricsManager/MetricsAgent/DAL/RamMetricsRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Data.SQLite;
+using Dapper;
 
 namespace MetricsAgent.DAL
 {
@@ -13,48 +14,45 @@ namespace MetricsAgent.DAL
     public class RamMetricsRepository : IRamMetricsRepository
     {
         private const string ConnectionString = "Data Source=metrics.db;Version=3;Pooling=true;Max Pool Size=100;";
+        public RamMetricsRepository()
+        {
+            SqlMapper.AddTypeHandler(new DateTimeOffsetHandler());
+        }
         public void Create(RamMetric item)
         {
-            using var connection = new SQLiteConnection(ConnectionString);
-            connection.Open();
-            using var cmd = new SQLiteCommand(connection);
-            cmd.CommandText = "INSERT INTO rammetrics(value, time) VALUES(@value, @time)";
-            cmd.Parameters.AddWithValue("@value", item.Value);
-            cmd.Parameters.AddWithValue("@time", item.Time.ToUnixTimeSeconds());
-            cmd.Prepare();
-            cmd.ExecuteNonQuery();
+            using (var connection = new SQLiteConnection(ConnectionString))
+            {
+                connection.Execute("INSERT INTO rammetrics(value, time) VALUES(@value, @time)",
+                    new
+                    {
+                        value = item.Value,
+                        time = item.Time.ToUnixTimeSeconds()
+                    });
+            };
         }
 
         public IList<RamMetric> GetByTimePeriod(DateTimeOffset fromTime, DateTimeOffset toTime)
         {
-            using var connection = new SQLiteConnection(ConnectionString);
-            connection.Open();
-            using var cmd = new SQLiteCommand(connection);
-            var returnArray = new List<RamMetric>();
-            cmd.CommandText = "SELECT * FROM rammetrics WHERE time Between @time1 AND @time2";
-            cmd.Parameters.AddWithValue("@time1", fromTime.ToUnixTimeSeconds());
-            cmd.Parameters.AddWithValue("@time2", toTime.ToUnixTimeSeconds());
-            using (SQLiteDataReader reader = cmd.ExecuteReader())
+            using (var connection = new SQLiteConnection(ConnectionString))
             {
-                while (reader.Read())
-                {
-                    returnArray.Add(new RamMetric
-                    {
-                        Id = reader.GetInt32(0),
-                        Value = reader.GetInt32(1),
-                        Time = DateTimeOffset.FromUnixTimeSeconds(reader.GetInt32(2))
-                    });
-                }
-                return returnArray;
+                return connection.Query<RamMetric>("SELECT Id, Time, Value FROM rammetrics WHERE time Between @time1 AND @time2",
+                    new { time1 = fromTime, time2 = toTime }).ToList();
             }
         }
         public IList<RamMetric> GetAll()
         {
-            return new List<RamMetric>();
+            using (var connection = new SQLiteConnection(ConnectionString))
+            {
+                return connection.Query<RamMetric>("SELECT Id, Time, Value FROM rammetrics").ToList();
+            }
         }
         public RamMetric GetById(int id)
         {
-            return new RamMetric();
+            using (var connection = new SQLiteConnection(ConnectionString))
+            {
+                return connection.QuerySingle<RamMetric>("SELECT Id, Time, Value FROM rammetrics WHERE id=@id",
+                    new { id = id });
+            }
         }
     }
 }
---
+++ MetricsManager/MetricsAgent/MapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MetricsAgent.DAL;
+using MetricsAgent.Responses;
+
+namespace MetricsAgent
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<CpuMetric, CpuMetricDto>();
+            CreateMap<RamMetric, RamMetricDto>();
+            CreateMap<HddMetric, HddMetricDto>();
+            CreateMap<DotNetMetric, DotNetMetricDto>();
+            CreateMap<NetworkMetric, NetworkMetricDto>();
+        }
+    }
+}
